5de39460aa438869f2d1d78533e14fda8c5c07c7
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-10-27 18:42:44 -0400

    v

--- src/hooks/site/wear/useSitesWearWithCursor.ts
+++ src/hooks/site/wear/useSitesWearWithCursor.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { ConnectionArgs, ListResponse, Product } from "../../../../interfaces";
-import { FIND_SITES_WITH_CURSOR, graphQLClient } from '../../../../graphql';
+import { FIND_SITES_WEAR_WITH_CURSOR, graphQLClient } from '../../../../graphql';
 
 export const findSitesWearWithCursor = async (args:ConnectionArgs) => {
-  const { listSitesWithCursor } = await graphQLClient.request(FIND_SITES_WITH_CURSOR, {args: args});
+  const { listSitesWithCursor } = await graphQLClient.request(FIND_SITES_WEAR_WITH_CURSOR, {args: args});
   return listSitesWithCursor;
 };
 
